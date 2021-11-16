b7546376b94835ad9175303cceff711a5f1f3a44
serac8 <92148837+serac8@users.noreply.github.com>
2021-11-16 12:40:12 +0100

    01 - Commit inicial

---
+++ TiendaCoches.java
@@ -0,0 +1,59 @@
+public class TiendaCoches {
+
+    private String propietario;
+    private int litrosActualesDeGasolina;
+    private boolean motorArrancado;
+   
+
+
+    public TiendaCoches(String nombrePropietario, int litros, boolean arrancado) {
+        propietario = nombrePropietario;
+        litrosActualesDeGasolina = litros;
+        motorArrancado = arrancado;
+    }
+
+   
+   public String getnombrePropietario() {
+       return propietario; 
+    }
+
+    public int getlitros() {
+        return litrosActualesDeGasolina;
+    }
+   
+    public boolean motorArrancado() {
+        if(motorArrancado == true) {
+            System.out.println("Coche arrancado");
+            
+        }
+        else {
+            System.out.println("Coche apagado");
+        }
+    return motorArrancado;
+    }
+    
+    public String setpropietario() {
+            return propietario;
+    }
+    
+    public void repostarGasolina(int litrosRepostados) {
+        litrosActualesDeGasolina = litrosActualesDeGasolina + litrosRepostados; 
+    }
+    
+    public void girarLlave() {
+        if(motorArrancado == true) {
+            motorArrancado = false;
+        }
+        else {
+            motorArrancado = true;
+        }
+    }
+    
+    public void imprimeDetalles() {
+        System.out.println("Propietario:" + propietario + " | " + "Estado del depósito de gasolina:" + litrosActualesDeGasolina + " | " + "Motor arrancado:" + motorArrancado);
+    }
+    
+    public boolean estadoDelVehículo() {
+        return motorArrancado;
+    }
+}
